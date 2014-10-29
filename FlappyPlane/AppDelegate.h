9434c1833b816750cb383511bdf7826e23b58435
kenwilcox <wilcoxk@gmail.com>
2014-10-29 06:39:43 -0600

    Initial Commit

---
+++ FlappyPlane/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlappyPlane
+//
+//  Created by Kenneth Wilcox on 10/29/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlappyPlane/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  FlappyPlane
+//
+
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
