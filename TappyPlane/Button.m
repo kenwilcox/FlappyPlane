875087657fac535dad77a19dd8b9971d22021c7b
kenwilcox <wilcoxk@gmail.com>
2014-11-23 21:42:18 -0700

    initial methods

--- TappyPlane/Button.m
+++ TappyPlane/Button.m
@@ -10,4 +10,28 @@
 
 @implementation Button
 
++ (instancetype)spriteNodeWithTexture:(SKTexture *)texture
+{
+  Button *instance = [super spriteNodeWithTexture:texture];
+  instance.pressedScale = 0.9;
+  instance.userInteractionEnabled = YES;
+  return instance;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+}
+
 @end
