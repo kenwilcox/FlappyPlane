9e5bc17cbcc14f01a39486a7d4e78a6ff48b09ff
kenwilcox <wilcoxk@gmail.com>
2014-11-23 21:36:43 -0700

    Start of Button Class

---
+++ TappyPlane/Button.h
@@ -0,0 +1,15 @@
+//
+//  Button.h
+//  TappyPlane
+//
+//  Created by Kenneth Wilcox on 11/23/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Button : SKSpriteNode
+
+@property (nonatomic) CGFloat pressedScale;
+
+@end
---
+++ TappyPlane/Button.m
@@ -0,0 +1,13 @@
+//
+//  Button.m
+//  TappyPlane
+//
+//  Created by Kenneth Wilcox on 11/23/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import "Button.h"
+
+@implementation Button
+
+@end
