ff64282e242764ef5157242611ba4a9ea530deee
kenwilcox <wilcoxk@gmail.com>
2014-11-25 20:51:47 -0700

    added GameOverMenu

---
+++ TappyPlane/GameOverMenu.h
@@ -0,0 +1,13 @@
+//
+//  GameOverMenu.h
+//  TappyPlane
+//
+//  Created by Kenneth Wilcox on 11/25/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverMenu : SKNode
+
+@end
---
+++ TappyPlane/GameOverMenu.m
@@ -0,0 +1,13 @@
+//
+//  GameOverMenu.m
+//  TappyPlane
+//
+//  Created by Kenneth Wilcox on 11/25/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import "GameOverMenu.h"
+
+@implementation GameOverMenu
+
+@end
