d449a10978df66c12adc568c3251321dc6b57700
kenwilcox <wilcoxk@gmail.com>
2014-11-26 22:45:20 -0700

    Added GameOverMenu panelBackground

--- TappyPlane/GameOverMenu.m
+++ TappyPlane/GameOverMenu.m
@@ -16,7 +16,17 @@
     return nil;
   _size = size;
   
+  SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:@"Graphics"];
   
+  SKSpriteNode *panelBackground = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"UIbg"]];
+  panelBackground.position = CGPointMake(size.width * 0.5, size.height - 150.00);
+  CGFloat width = panelBackground.size.width;
+  CGFloat height = panelBackground.size.height;
+  panelBackground.centerRect = CGRectMake((10 / width), (10 / height), ((width -20) / width) ,((height -20) / height));
+  panelBackground.xScale = 175.0 / width;
+  panelBackground.yScale = 155.0 / height;
+
+  [self addChild:panelBackground];
   
   return self;
 }
