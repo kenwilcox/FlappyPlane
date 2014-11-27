bba656d225e224cf015c9d4f94bae836bd318611
kenwilcox <wilcoxk@gmail.com>
2014-11-26 23:04:18 -0700

    Added panelGroup, score, best and medal

--- TappyPlane/GameOverMenu.m
+++ TappyPlane/GameOverMenu.m
@@ -18,15 +18,37 @@
   
   SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:@"Graphics"];
   
+  // Setup node to act as a group for panel elements
+  SKNode *panelGroup = [SKNode node];
+  [self addChild:panelGroup];
+  
+  // Setup background panel
   SKSpriteNode *panelBackground = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"UIbg"]];
   panelBackground.position = CGPointMake(size.width * 0.5, size.height - 150.00);
   CGFloat width = panelBackground.size.width;
   CGFloat height = panelBackground.size.height;
   panelBackground.centerRect = CGRectMake((10 / width), (10 / height), ((width -20) / width) ,((height -20) / height));
   panelBackground.xScale = 175.0 / width;
-  panelBackground.yScale = 155.0 / height;
-
-  [self addChild:panelBackground];
+  panelBackground.yScale = 115.0 / height;
+  [panelGroup addChild:panelBackground];
+  
+  // Setup score title
+  SKSpriteNode *scoreTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textScore"]];
+  scoreTitle.anchorPoint = CGPointMake(1.0, 1.0);
+  scoreTitle.position = CGPointMake(CGRectGetMaxX(panelBackground.frame) -20, CGRectGetMaxY(panelBackground.frame) -10);
+  [panelGroup addChild:scoreTitle];
+  
+  // Setup best title
+  SKSpriteNode *bestTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textBest"]];
+  bestTitle.anchorPoint = CGPointMake(1.0, 1.0);
+  bestTitle.position = CGPointMake(CGRectGetMaxX(panelBackground.frame) -20, CGRectGetMaxY(panelBackground.frame) -60);
+  [panelGroup addChild:bestTitle];
+  
+  // Setup medal title
+  SKSpriteNode *medalTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textMedal"]];
+  medalTitle.anchorPoint = CGPointMake(0.0, 1.0);
+  medalTitle.position = CGPointMake(CGRectGetMinX(panelBackground.frame) +20, CGRectGetMaxY(panelBackground.frame) -10);
+  [panelGroup addChild:medalTitle];
   
   return self;
 }
