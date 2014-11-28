664c4fe67feea7ff089160cc17748b43d965f7ee
kenwilcox <wilcoxk@gmail.com>
2014-11-27 22:29:53 -0700

    animate menu panel

--- TappyPlane/GameOverMenu.m
+++ TappyPlane/GameOverMenu.m
@@ -16,6 +16,7 @@
 @property (nonatomic) BitmapFontLabel *scoreText;
 @property (nonatomic) BitmapFontLabel *bestScoreText;
 @property (nonatomic) SKSpriteNode *gameOverTitle;
+@property (nonatomic) SKNode *panelGroup;
 @end
 
 @implementation GameOverMenu
@@ -34,8 +35,8 @@
   [self addChild:_gameOverTitle];
   
   // Setup node to act as a group for panel elements
-  SKNode *panelGroup = [SKNode node];
-  [self addChild:panelGroup];
+  _panelGroup = [SKNode node];
+  [self addChild:_panelGroup];
   
   // Setup background panel
   SKSpriteNode *panelBackground = [SKSpriteNode spriteNodeWithTexture:[_atlas textureNamed:@"UIbg"]];
@@ -45,45 +46,45 @@
   panelBackground.centerRect = CGRectMake((10 / width), (10 / height), ((width -20) / width) ,((height -20) / height));
   panelBackground.xScale = 175.0 / width;
   panelBackground.yScale = 115.0 / height;
-  [panelGroup addChild:panelBackground];
+  [self.panelGroup addChild:panelBackground];
   
   // Setup score title
   SKSpriteNode *scoreTitle = [SKSpriteNode spriteNodeWithTexture:[_atlas textureNamed:@"textScore"]];
   scoreTitle.anchorPoint = CGPointMake(1.0, 1.0);
   scoreTitle.position = CGPointMake(CGRectGetMaxX(panelBackground.frame) -20, CGRectGetMaxY(panelBackground.frame) -10);
-  [panelGroup addChild:scoreTitle];
+  [self.panelGroup addChild:scoreTitle];
   
   // Setup score text label
   _scoreText = [[BitmapFontLabel alloc] initWithText:@"0" andFontName:@"number"];
   _scoreText.alignment = BitmapFontAlignmentRight;
   _scoreText.position = CGPointMake(CGRectGetMaxX(scoreTitle.frame), CGRectGetMinY(scoreTitle.frame) - 15);
   [_scoreText setScale:0.5];
-  [panelGroup addChild:_scoreText];
+  [self.panelGroup addChild:_scoreText];
   
   // Setup best title
   SKSpriteNode *bestTitle = [SKSpriteNode spriteNodeWithTexture:[_atlas textureNamed:@"textBest"]];
   bestTitle.anchorPoint = CGPointMake(1.0, 1.0);
   bestTitle.position = CGPointMake(CGRectGetMaxX(panelBackground.frame) - 20, CGRectGetMaxY(panelBackground.frame) - 60);
-  [panelGroup addChild:bestTitle];
+  [self.panelGroup addChild:bestTitle];
   
   // Setup bestscore text label
   _bestScoreText = [[BitmapFontLabel alloc] initWithText:@"0" andFontName:@"number"];
   _bestScoreText.alignment = BitmapFontAlignmentRight;
   _bestScoreText.position = CGPointMake(CGRectGetMaxX(bestTitle.frame), CGRectGetMinY(bestTitle.frame) - 15);
   [_bestScoreText setScale:0.5];
-  [panelGroup addChild:_bestScoreText];
+  [self.panelGroup addChild:_bestScoreText];
   
   // Setup medal title
   SKSpriteNode *medalTitle = [SKSpriteNode spriteNodeWithTexture:[_atlas textureNamed:@"textMedal"]];
   medalTitle.anchorPoint = CGPointMake(0.0, 1.0);
   medalTitle.position = CGPointMake(CGRectGetMinX(panelBackground.frame) + 20, CGRectGetMaxY(panelBackground.frame) - 10);
-  [panelGroup addChild:medalTitle];
+  [self.panelGroup addChild:medalTitle];
   
   // Setup display of medal
   _medalDisplay = [SKSpriteNode spriteNodeWithTexture:[_atlas textureNamed:@"medalBlank"]];
   _medalDisplay.anchorPoint = CGPointMake(0.5, 1.0);
   _medalDisplay.position = CGPointMake(CGRectGetMidX(medalTitle.frame), CGRectGetMinY(medalTitle.frame) - 15);
-  [panelGroup addChild:_medalDisplay];
+  [self.panelGroup addChild:_medalDisplay];
   
   // Setup play button
   Button *playButton = [Button spriteNodeWithTexture:[_atlas textureNamed:@"buttonPlay"]];
@@ -142,5 +143,13 @@
   dropGameOverText.timingMode = SKActionTimingEaseOut;
   self.gameOverTitle.position = CGPointMake(self.gameOverTitle.position.x, self.gameOverTitle.position.y + 100);
   [self.gameOverTitle runAction:dropGameOverText];
+  
+  // Animate main menu panel
+  // group - at the same time, sequence - one after the other
+  SKAction *raisePanel = [SKAction group:@[[SKAction fadeInWithDuration:0.4],[SKAction moveByX:0.0 y:100 duration:0.4]]];
+  raisePanel.timingMode = SKActionTimingEaseOut;
+  self.panelGroup.alpha = 0.0;
+  self.panelGroup.position = CGPointMake(self.panelGroup.position.x, self.panelGroup.position.y - 100);
+  [self.panelGroup runAction:[SKAction sequence:@[[SKAction waitForDuration:0.7], raisePanel]]];
 }
 @end
